dec03418577605f23fd2171907ddc54aa56ea81b
staxx1 <66185417+staxx1@users.noreply.github.com>
2020-09-07 16:11:41 +0300

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [FRIDAY](https://www.github.com/midhunkm1294-bit/FRIDAY)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [TURHAN](https://github.com/staxx1/TurhanUser/)>> ",
 
          force_document=True,
 
@@ -160,7 +160,7 @@ async def carbon_api(e):
 
          )
 
-   os.remove('./FRIDAY.png')
+   os.remove('./TURHAN.png')
 
    driver.quit()
 
