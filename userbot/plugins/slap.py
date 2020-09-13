7dbda3b92654534663d41e37addc00c6066d5ee9
staxx1 <66185417+staxx1@users.noreply.github.com>
2020-09-13 20:29:32 +0300

    Update slap.py

--- userbot/plugins/slap.py
+++ userbot/plugins/slap.py
@@ -73,7 +73,7 @@ HIT = [
     "bashes",
 ]
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "FridayUserbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TurhanUserbot"
 
 @borg.on(admin_cmd(pattern="slap ?(.*)", allow_sudo=True))
 async def who(event):
