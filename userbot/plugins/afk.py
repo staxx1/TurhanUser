2987914ea1620fc3a895cb506d7fd6b4c9f5ede8
staxx1 <66185417+staxx1@users.noreply.github.com>
2020-09-20 01:36:13 +0300

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -24,7 +24,7 @@ async def set_not_afk(event):
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PLUGIN_CHANNEL,  # pylint:disable=E0602
-                "#AfkLogger My Boss Went Afk"
+                "#AfkLogger Sahibim şuan afk"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
