555e7e7a781104981b92e33bb9ad583b062bc14d
staxx1 <66185417+staxx1@users.noreply.github.com>
2021-01-10 15:25:44 +0300

    Add files via upload

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -1,57 +1,46 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-from asyncio import sleep
+"""Invite the user(s) to the current chat
+Syntax: .invite <User(s)>"""
 
 from telethon import functions
 
-from userbot import CMD_HELP
-from userbot.events import register
+from telebot import CMD_HELP
+from telebot.utils import admin_cmd
 
 
-@register(outgoing=True, pattern=r"^\.invite(?: |$)(.*)")
+@telebot.on(admin_cmd(pattern="invite ?(.*)"))
+@telebot.on(sudo_cmd(pattern="invite ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     to_add_users = event.pattern_match.group(1)
     if event.is_private:
-        await event.edit("`.invite` users to a chat, not to a Private Message")
+        await eor(event, "`.invite` users to a chat, not to a Private Message")
     else:
+        logger.info(to_add_users)
         if not event.is_channel and event.is_group:
             # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
             for user_id in to_add_users.split(" "):
                 try:
-                    await event.client(
+                    await borg(
                         functions.messages.AddChatUserRequest(
                             chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000
                         )
                     )
                 except Exception as e:
-                    await event.edit(str(e))
-                    return
+                    await event.reply(str(e))
+            await eor(event, "Invited Successfully")
         else:
             # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
             for user_id in to_add_users.split(" "):
                 try:
-                    await event.client(
+                    await borg(
                         functions.channels.InviteToChannelRequest(
                             channel=event.chat_id, users=[user_id]
                         )
                     )
                 except Exception as e:
-                    await event.edit(str(e))
-                    return
-
-        await event.edit("`Invited Successfully`")
-        await sleep(2)
-        await event.delete()
+                    await event.reply(str(e))
+            await eor(event, "TeleBot has invited the user to the chat.")
 
 
-CMD_HELP.update(
-    {
-        "invite": ">`.invite` <username> \
-        \nUsage: Invite some user or bots if u want."
-    }
-)
+CMD_HELP.update({"invite": ".invite <username>\nUse - Add user to the chat."})
